8e6cb47507de02d6063a597ca269d13e25a09559
Kailang Fu <kailangfu@gmail.com>
2017-10-21 18:01:42 -0500

    Add more views
    Almost finished.
    Head for dinner.

--- routes/exchange.js
+++ routes/exchange.js
@@ -25,4 +25,32 @@ router.get('/watchlist', function(req, res, next) {
 	res.render('exchange/watchlist', { title: res.__('common-gskse') });
 });
 
+router.get('/markets', function(req, res, next) {
+	res.render('exchange/markets', { title: res.__('common-gskse') });
+});
+
+router.get('/trade', function(req, res, next) {
+	res.render('exchange/trade', { title: res.__('common-gskse'), body: req.body });
+});
+
+router.post('/trade', function(req, res, next) {
+	res.render('exchange/trade', { title: res.__('common-gskse'), body: req.body });
+});
+
+router.get('/trade/open', function(req, res, next) {
+	res.render('exchange/trade_open', { title: res.__('common-gskse') });
+});
+
+router.get('/trade/failed', function(req, res, next) {
+	res.render('exchange/trade_failed', { title: res.__('common-gskse') });
+});
+
+router.get('/trade/history', function(req, res, next) {
+	res.render('exchange/portfolio_trade', { title: res.__('common-gskse') });
+});
+
+router.get('/quote/:symbol', function(req, res, next) {
+	res.render('exchange/quote', { title: res.__('common-gskse') });
+});
+
 module.exports = router;
